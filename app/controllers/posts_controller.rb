4adbc47257bc25723eb2f6fe9513213f36e4c96c
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-06-03 00:17:14 +0500

    Add Posts Model, Controller, and Views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  # GET /posts or /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts or /posts.json
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to posts_path, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:new, :create, :index]
   get 'home/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
